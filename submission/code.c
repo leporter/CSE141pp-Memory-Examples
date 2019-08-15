eb2e1dab5b088f536ffda8b840f0ffa75e8f950c
Devon <9127315+djmerrill@users.noreply.github.com>
2019-08-15 08:48:00 -0700

    Update code.c

--- submission/code.c
+++ submission/code.c
@@ -18,8 +18,8 @@ void sqmm(double * d, double * a, double * b, uint64_t size)
 			for (uint64_t i = 0; i < size; i += 1) {
 				/*
 				Multiply row of first matrix to column of second matrix
-             	and store sum of product of elements in sum.
-             	*/
+				and store sum of product of elements in sum.
+				*/
 				sum += a[r + size*i] * b[i + size*c];
 			}
 			d[r + size*c] = sum;
