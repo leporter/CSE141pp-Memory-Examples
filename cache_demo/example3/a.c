ea0d6e73535f9dc22eccc9b9deb475208c78d2d8
leporter <leporter@eng.ucsd.edu>
2019-08-20 02:46:23 +0000

    added cache_demo

---
+++ cache_demo/example3/a.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_index(int x, int y, int rows, int cols){
+	return y*cols + x;
+}
+
+int main() {
+  int rows = 10000;
+  int cols = 10000;
+  int iterations = 10;
+  int val = 1;
+
+  int count = 0;
+  int *arr = (int *)malloc(rows * cols * sizeof(int));  
+
+  for(int count = 0; count<iterations; count++){
+	  for(int x = 0; x < cols; x++) {
+		  for(int y = 0; y < rows; y++) {
+			(*(arr + y*cols + x))++;
+			val+=*(arr + y*cols + x);
+		  }
+	  }
+  }
+  printf("Val = %d\n",val);
+  free(arr);  
+
+}
